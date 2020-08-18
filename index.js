56729ee24becc7d5ee0f30a406a7c644d4709739
Mice <rykov.nikas@gmail.com>
2020-08-18 05:32:11 +0300

    hots added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+global.bases = require("./bases.json")
+global.bullets = require("./bullets.json")
+global.fps = 30;
 (async () => {
   let express = require("express")
   let app = express()
   let http = require('http').createServer(app);
   let io = require("socket.io")(http)
+
   app.use(express.static(__dirname + "/static"))
   http.listen(3000, () => {
 
@@ -17,16 +21,21 @@
       let player = {
         type: "PLAYER",
         base: "tank",
+
+        velocity: {
+          x: 0,
+          y: 0
+        },
+        lastShot: 0,
+        shotting: false,
         xp: 1,
-        velocity:{x:0,y:0},
-        speed:10,
-        hp: 30,
         rotation: 0,
         nickname: nickname,
         id: genId("player"),
         x: Math.rand(10, 9990),
         y: Math.rand(10, 9990),
-        me: false
+        me: false,
+        ...bases.tank
       }
       socket.id = player.id
       players.forEach(connection => {
@@ -35,14 +44,17 @@
       objects.push(player)
       players.push(socket)
 
-      socket.emit("spawn", {...player,me:true})
+      socket.emit("spawn", {
+        ...player,
+        me: true
+      })
       objects.filter(o => o.id !== player.id).forEach(o => {
         socket.emit("spawn", o)
       })
       setInterval(() => {
         players.forEach(p => {
           p.emit("leaders", (() => {
-            return objects.sort((a, b) => b.xp - a.xp).slice(0, 10).map((l, li) => `${li+1}. ${l.nickname} - ${l.xp}xp <br>`).join("")
+            return objects.filter(o=>o.type=="PLAYER").sort((a, b) => b.xp - a.xp).slice(0, 10).map((l, li) => `${li+1}. ${l.nickname} - ${l.xp}xp <br>`).join("")
           })())
         })
       }, 1000)
@@ -58,59 +70,117 @@
           })
         })
       })
-      socket.on("move-x",(move)=>{
-          let objIndex = objects.findIndex(o => o.id == player.id)
-        if(move==0){
-            objects[objIndex].velocity.x=0
-        }else if(move>0){
-            objects[objIndex].velocity.x=  objects[objIndex].speed
-        }else{
-          objects[objIndex].velocity.x= -objects[objIndex].speed
+      socket.on("shotting", v => {
+
+        let objIndex = objects.findIndex(o => o.id == player.id)
+        objects[objIndex].shotting = Boolean(v)
+
+      })
+      socket.on("move-x", (move) => {
+        let objIndex = objects.findIndex(o => o.id == player.id)
+        if (move == 0) {
+          objects[objIndex].velocity.x = 0
+        } else if (move > 0) {
+          objects[objIndex].velocity.x = objects[objIndex].speed
+        } else {
+          objects[objIndex].velocity.x = -objects[objIndex].speed
         }
       })
-      socket.on("move-y",(move)=>{
-          let objIndex = objects.findIndex(o => o.id == player.id)
-        if(move==0){
-            objects[objIndex].velocity.y=0
-        }else if(move>0){
-            objects[objIndex].velocity.y=  objects[objIndex].speed
-        }else{
-          objects[objIndex].velocity.y= -objects[objIndex].speed
+      socket.on("move-y", (move) => {
+        let objIndex = objects.findIndex(o => o.id == player.id)
+        if (move == 0) {
+          objects[objIndex].velocity.y = 0
+        } else if (move > 0) {
+          objects[objIndex].velocity.y = objects[objIndex].speed
+        } else {
+          objects[objIndex].velocity.y = -objects[objIndex].speed
         }
       })
-      socket.on("chat-message",(message)=>{
-        players.forEach(connection=>{connection.emit("chat-message",player.id,message)})
+
+      socket.on("chat-message", (message) => {
+        players.forEach(connection => {
+          connection.emit("chat-message", player.id, message)
+        })
       })
-      socket.on("disconnect",()=>{
-          let objIndex = objects.findIndex(o => o.id == player.id)
-          objects.splice(objIndex,1)
-          players.forEach((pl) => {
-            pl.emit("base-killed",player.id)
-          });
+      socket.on("disconnect", () => {
+        let objIndex = objects.findIndex(o => o.id == player.id)
+        objects.splice(objIndex, 1)
+        players.forEach((pl) => {
+          pl.emit("destroy", player.id)
+        });
 
       })
     })
   })
 })()
-setInterval(async ()=>{
-let needUpdate=  objects.filter(o=>(o.velocity.y!==0||o.velocity.x!==0))
+setInterval(async () => {
+  let needUpdate = objects.filter(o => (o.velocity.y !== 0 || o.velocity.x !== 0))
+// console.log(needUpdate);
+  let upg=needUpdate.map((ob, obi) => {
 
-needUpdate.forEach((ob,obi)=>{
+    let rid = objects.findIndex(o => o.id == ob.id)
 
-  let rid=objects.findIndex(o=>o.id==ob.id)
+    if (rid == -1) {
+      return
+    }
+    objects[rid].x += ob.velocity.x
+    objects[rid].y += ob.velocity.y
+    if (objects[rid].x < 10) {
+      objects[rid].x = 9990
+    } else if (objects[rid].x > 9990) {
+      objects[rid].x = 10
+    }
+    if (objects[rid].y < 10) {
+      objects[rid].y = 9990
+    } else if (objects[rid].y > 9990) {
+      objects[rid].y = 10
+    }
+    return {id:ob.id,upgrade:{
+      position: {
+        x: objects[rid].x,
+        y: objects[rid].y
+      }
+    }}
 
-  if(rid==-1){return}
-    objects[rid].x+=ob.velocity.x
-    objects[rid].y+=ob.velocity.y
-    if(objects[rid].x<10){objects[rid].x=9990}
-    else if(objects[rid].x>9990){objects[rid].x=10}
-    if(objects[rid].y<10){objects[rid].y=9990}
-    else if(objects[rid].y>9990){objects[rid].y=10}
-    players.forEach(async player=>{
-      player.emit("upgrade",ob.id,{position:{x:objects[rid].x,y:objects[rid].y}})
-    })
   })
-},1000/40)
+    players.forEach(c=>c.emit("upgrades",upg))
+  let shotting = objects.filter(o => (o.shotting == true && o.lastShot + o.shotTimeout < Date.now()))
+  shotting.forEach((ob, obi) => {
+    let rid = objects.findIndex(o => o.id == ob.id)
+
+    if (rid == -1) {
+      return
+    }
+    objects[rid].lastShot=Date.now()
+    let rotation=ob.rotation+((Math.random()-Math.random())/2)
+    let bullet = {
+      type: "BULLET",
+      rotation:rotation,
+      id:genId("bullet"),
+      from:ob.id,
+      ...bullets[ob.bullet],
+      x:ob.x-Math.cos(ob.rotation)*50,
+      y:ob.y-Math.sin(ob.rotation)*50,
+    velocity:{
+        x:-Math.cos(rotation)*bullets[ob.bullet].speed,
+        y:-Math.sin(rotation)*bullets[ob.bullet].speed
+      }
+    }
+    objects.push(bullet)
+  players.forEach(c=>c.emit("spawn",bullet))
+  objects[rid].x=ob.x+Math.round(Math.cos(ob.rotation)*bullets[ob.bullet].recoil)
+  objects[rid].y=ob.y+Math.round(Math.sin(ob.rotation)*bullets[ob.bullet].recoil)
+    players.forEach(c=>c.emit("upgrade",ob.id,{position:{
+      x:objects[rid].x,
+      y:objects[rid].y
+    }}))
+  setTimeout(()=>{
+      players.forEach(c=>c.emit("destroy",bullet.id))
+
+      objects.splice(objects.findIndex(o=>o.id==bullet.id),1)
+  },bullet.alive)
+  })
+}, 1000 / fps)
 Math.rand = function(min, max) {
 
   let rand = min + Math.random() * (max + 1 - min);
--- static/game.js
+++ static/game.js
@@ -1,164 +1,239 @@
-let nickname=localStorage.getItem("nickname")
-if(nickname){
-  document.getElementById('nickname').value=nickname
+let nickname = localStorage.getItem("nickname")
+if (nickname) {
+  document.getElementById('nickname').value = nickname
 }
-let objects=[]
+let objects = []
 let me;
-window.messages=[]
-document.getElementById('nickname').oninput=function(){
-  nickname=document.getElementById('nickname').value
-  localStorage.setItem("nickname",document.getElementById('nickname').value)
+window.messages = []
+document.getElementById('nickname').oninput = function() {
+  nickname = document.getElementById('nickname').value
+  localStorage.setItem("nickname", document.getElementById('nickname').value)
 }
-async function startGame(){
-  window.app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-  document.getElementById("welcome").style.display="none"
-  document.getElementById("interface").style.display="inline-block"
-  app.view.id="game"
+async function startGame() {
+  window.app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  document.getElementById("welcome").style.display = "none"
+  document.getElementById("interface").style.display = "inline-block"
+  app.view.id = "game"
   app.renderer.backgroundColor = 0x575757
   document.body.appendChild(app.view);
   PIXI.Loader.shared
-  .add([
-    "/assets/druon-grid.png",
-    "/assets/bases/tank.svg"
-  ])
-  .load(play);
+    .add([
+      "/assets/druon-grid.png",
+      "/assets/skins/tank.svg",
+      "/assets/bullets/bullet-fire.svg"
+    ])
+    .load(play);
 }
 async function play() {
-  const tilingSprite = new PIXI.TilingSprite(
-      PIXI.Loader.shared.resources["/assets/druon-grid.png"].texture,
-      10000,
-      10000,
+  const background = new PIXI.TilingSprite(
+    PIXI.Loader.shared.resources["/assets/druon-grid.png"].texture,
+    10000,
+    10000,
   );
-  app.stage.addChild(tilingSprite);
-  app.stage.pivot.set(5000,5000)
-let socket=io("/game")
-socket.emit("spawn",nickname)
-socket.on("spawn",handleSpawn)
-app.stage.interactive=true
-app.stage.on("mousemove",(event)=>{
-  var dist_Y = app.screen.height/2 - event.data.global.y;
-    var dist_X = app.screen.width/2 - event.data.global.x;
-    var angle = Math.atan2(dist_Y,dist_X);
+  app.stage.addChild(background);
+  app.stage.pivot.set(5000, 5000)
+  let socket = io("/game")
+  socket.emit("spawn", nickname)
 
-    socket.emit("rotate",angle)
-})
-socket.on("upgrade",(id,upgrade)=>{
-  let sprite=objects.find(o=>o.data.id==id).sprite
-  Object.keys(upgrade).forEach(up=>{
-    sprite[up]=upgrade[up]
+  socket.on("spawn", handleSpawn)
+  app.stage.interactive = true
+
+  app.stage.on("mousemove", (event) => {
+    var dist_Y = app.screen.height / 2 - event.data.global.y;
+    var dist_X = app.screen.width / 2 - event.data.global.x;
+    var angle = Math.atan2(dist_Y, dist_X);
+
+    socket.emit("rotate", angle)
   })
-  if(upgrade.position){
-    sprite.nickname.position.x=sprite.position.x;
-    sprite.nickname.position.y=sprite.position.y+50;
-  }
-})
-socket.on("chat-message",(id,message)=>{
-  let style = new PIXI.TextStyle({
 
-   fill: "black",
-   fontFamily: "Oxygen-Regular",
-   fontSize: 18
-});
-let text = new PIXI.Text(message, style);
-text.anchor.set(0.5,0.5)
-let author=objects.find(obj=>obj.data.id==id).sprite
-text.position.x=author.position.x;
-text.position.y=author.position.y-50;
-text.author=id
-app.stage.addChild(text)
-messages.push(text)
-setTimeout(()=>{
-  messages.splice(messages.findIndex(m=>m.author==id))
-
-  app.stage.removeChild(text)
-},5000)
-})
-socket.on("leaders",(leaders)=>{document.getElementById("leaderboard").innerHTML="<h1>Leaderboard</h1>"+leaders})
-let chatting=false
-window.onkeydown=async(ev)=>{
-  if(chatting){return handleChatting(ev)}
-if(ev.code=="KeyA"){
-  socket.emit("move-x",-1)
-}else if(ev.code=="KeyD"){
-    socket.emit("move-x",1)
-}
-else if(ev.code=="KeyW"){
-    socket.emit("move-y",-1)
-}else if(ev.code=="KeyS"){
-    socket.emit("move-y",1)
-}else if(ev.code=="Enter"){
-    let chat=document.getElementById("chat")
-    chat.style.display="block"
-    chat.focus()
-    chatting=true
-}
-}
-async function handleChatting(ev){
-  if(ev.code=="Enter"){
-      let chat=document.getElementById("chat")
-    let message=chat.value;
-    chat.value=""
-    chat.style.display="none"
-chatting=false
-    if(message.trim()==""){return}
-    socket.emit("chat-message",message)
-  }
-}
-window.onkeyup=async (ev)=>{
-  if(ev.code=="KeyA"||ev.code=="KeyD"){
-    socket.emit("move-x",0)
-  }
-  else if(ev.code=="KeyW"||ev.code=="KeyS"){
-      socket.emit("move-y",0)
-  }
-}
-
-app.ticker.add(async ()=>{
-  messages.forEach(async message=>{
-    let author=objects.find(o=>o.data.id==message.author).sprite
-    console.log(author);
-    message.position.x=author.position.x;
-    message.position.y=author.position.y-50;
+  app.stage.on("pointerdown", (event) => {
+    socket.emit("shotting", true)
   })
-  if(!me){return}
+  app.stage.on("pointerup", (event) => {
+    socket.emit("shotting", false)
+  })
+  socket.on("upgrade", (id, upgrade) => {
+    let ob = objects.find(o => o.data.id == id)
+    let sprite = ob.sprite
+    if (!sprite) {
+      return
+    }
+    Object.keys(upgrade).forEach(up => {
+      sprite[up] = upgrade[up]
+    })
+    if (upgrade.position && ob.data.type == "PLAYER") {
+      sprite.nickname.position.x = sprite.position.x;
+      sprite.nickname.position.y = sprite.position.y + 50;
+    }
+  })
+  socket.on("upgrades", (upgrades) => {
+    upgrades.forEach(async (up) => {
 
-  app.stage.pivot.set(me.position.x-(app.screen.width/2),me.position.y-(app.screen.height/2))
-})
-setInterval(()=>{
-    if(!me){return}
-    document.getElementById("cords").innerHTML="X - "+me.position.x+" | Y - "+me.position.y
-},3000)
-}
 
-async function handleSpawn(obj){
-  switch (obj.type) {
-    case "PLAYER":
-    let texture=PIXI.Loader.shared.resources["/assets/bases/"+obj.base+".svg"].texture
-    let sprite=new PIXI.Sprite(texture)
-    sprite.width=sprite.height=100
-    sprite.rotation=obj.rotation
-    sprite.anchor.set(0.5,0.5)
-    sprite.position.set(obj.x,obj.y)
-    app.stage.addChild(sprite)
-    objects.push({data:obj,sprite:sprite})
+        let ob=objects.find(o=>o.data.id==up.id)
+        let sprite=ob.sprite
+        if(!sprite){return}
+        Object.keys(up.upgrade).forEach(u=>{
+          sprite[u]=up.upgrade[u]
+        })
+        if(up.upgrade.position&&ob.data.type=="PLAYER"){
+          sprite.nickname.position.x=sprite.position.x;
+          sprite.nickname.position.y=sprite.position.y+50;
+        }
+
+    })
+
+  })
+  socket.on("destroy", (id) => {
+    let index = objects.findIndex(o => o.data.id == id)
+    if (index == -1) {
+      return
+    }
+    app.stage.removeChild(objects[index].sprite)
+    objects.splice(index, 1)
+  })
+  socket.on("chat-message", (id, message) => {
     let style = new PIXI.TextStyle({
 
-     fill: "blue",
-     fontFamily: "Oxygen-Regular",
-     fontSize: 16
+      fill: "black",
+      fontFamily: "Oxygen-Regular",
+      fontSize: 18
     });
-    let text = new PIXI.Text(obj.nickname, style);
-    text.anchor.set(0.5,0.5)
-    app.stage.addChild(sprite)
-    text.position.x=sprite.position.x;
-    text.position.y=sprite.position.y+50;
-    sprite.nickname=text
+    let text = new PIXI.Text(message, style);
+    text.anchor.set(0.5, 0.5)
+    let author = objects.find(obj => obj.data.id == id).sprite
+    text.position.x = author.position.x;
+    text.position.y = author.position.y - 50;
+    text.author = id
     app.stage.addChild(text)
-    if(obj.me){
-      me=sprite
-      app.stage.pivot.set(obj.x-(app.screen.width/2),obj.y-(app.screen.height/2))
-      document.getElementById("cords").innerHTML="X - "+sprite.position.x+" | Y - "+sprite.position.y
+    messages.push(text)
+    setTimeout(() => {
+      messages.splice(messages.findIndex(m => m.author == id))
+
+      app.stage.removeChild(text)
+    }, 5000)
+  })
+  socket.on("leaders", (leaders) => {
+    document.getElementById("leaderboard").innerHTML = "<h1>Leaderboard</h1>" + leaders
+  })
+  let chatting = false
+  window.onkeydown = async (ev) => {
+    if (chatting) {
+      return handleChatting(ev)
     }
+    if (ev.code == "KeyA") {
+      socket.emit("move-x", -1)
+    } else if (ev.code == "KeyD") {
+      socket.emit("move-x", 1)
+    } else if (ev.code == "KeyW") {
+      socket.emit("move-y", -1)
+    } else if (ev.code == "KeyS") {
+      socket.emit("move-y", 1)
+    } else if (ev.code == "Enter") {
+      let chat = document.getElementById("chat")
+      chat.style.display = "block"
+      chat.focus()
+      chatting = true
+    }
+  }
+  async function handleChatting(ev) {
+    if (ev.code == "Enter") {
+      let chat = document.getElementById("chat")
+      let message = chat.value;
+      chat.value = ""
+      chat.style.display = "none"
+      chatting = false
+      if (message.trim() == "") {
+        return
+      }
+      socket.emit("chat-message", message)
+    }
+  }
+  window.onkeyup = async (ev) => {
+    if (ev.code == "KeyA" || ev.code == "KeyD") {
+      socket.emit("move-x", 0)
+    } else if (ev.code == "KeyW" || ev.code == "KeyS") {
+      socket.emit("move-y", 0)
+    }
+  }
+
+  app.ticker.add(async () => {
+    messages.forEach(async message => {
+      let author = objects.find(o => o.data.id == message.author).sprite
+      console.log(author);
+      message.position.x = author.position.x;
+      message.position.y = author.position.y - 50;
+    })
+    if (!me) {
+      return
+    }
+
+    app.stage.pivot.set(me.position.x - (app.screen.width / 2), me.position.y - (app.screen.height / 2))
+  })
+  setInterval(() => {
+    if (!me) {
+      return
+    }
+    document.getElementById("cords").innerHTML = "X - " + me.position.x + " | Y - " + me.position.y
+  }, 3000)
+}
+
+async function handleSpawn(obj) {
+  switch (obj.type) {
+    case "PLAYER":
+      (async () => {
+        let texture = PIXI.Loader.shared.resources["/assets/skins/" + obj.skin].texture
+        let sprite = new PIXI.Sprite(texture)
+        sprite.width = sprite.height = 100
+        sprite.rotation = obj.rotation
+        sprite.anchor.set(0.5, 0.5)
+        sprite.position.set(obj.x, obj.y)
+        app.stage.addChild(sprite)
+        objects.push({
+          data: obj,
+          sprite: sprite
+        })
+        let style = new PIXI.TextStyle({
+
+          fill: "blue",
+          fontFamily: "Oxygen-Regular",
+          fontSize: 16
+        });
+        let text = new PIXI.Text(obj.nickname, style);
+        text.anchor.set(0.5, 0.5)
+        app.stage.addChild(sprite)
+        text.position.x = sprite.position.x;
+        text.position.y = sprite.position.y + 50;
+        sprite.nickname = text
+        app.stage.addChild(text)
+        if (obj.me) {
+          me = sprite
+          app.stage.pivot.set(obj.x - (app.screen.width / 2), obj.y - (app.screen.height / 2))
+          document.getElementById("cords").innerHTML = "X - " + sprite.position.x + " | Y - " + sprite.position.y
+        }
+      })()
+
+      break;
+    case "BULLET":
+      (async () => {
+
+        let texture = PIXI.Loader.shared.resources["/assets/bullets/" + obj.skin].texture
+        let sprite = new PIXI.Sprite(texture)
+        sprite.width = sprite.height = 100
+        sprite.rotation = obj.rotation
+        sprite.anchor.set(0.5, 0.5)
+        sprite.width = sprite.height = obj.size
+        sprite.position.set(obj.x, obj.y)
+        app.stage.addChild(sprite)
+        objects.push({
+          data: obj,
+          sprite: sprite
+        })
+      })()
+
       break;
     default:
 
