402b894e60d9aaa4e215b2779046de2fc93cb0da
Mice <rykov.nikas@gmail.com>
2020-08-22 18:41:17 +0300

    update

--- classes.js
+++ classes.js
@@ -146,12 +146,12 @@ exports.Scene = class Scene {
 async function physicsTick(scene) {
   scene.gameObjects.forEach(async (obj, i) => {
     let canMove=await handleCollisions(obj)
-    if(canMove){
+
       if(!obj||!obj.data){return}
     let uV = calcV(obj.data.rotation, obj.data.rp, true)
     obj.plusProp("x", obj.data.velocity.x + Math.round(uV.x))
     obj.plusProp("y", obj.data.velocity.y + Math.round(uV.y))
-  }
+
     let exp = require("./psy_tests.js")(obj)
     exp.forEach((ev, i) => {
       if (ev.e) {
