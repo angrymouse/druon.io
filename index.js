4387940665d7fdd9d66fc564efa35382420a136d
Mice <rykov.nikas@gmail.com>
2020-08-26 15:38:26 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ global.bases = require("./bases.json")
 global.bullets = require("./bullets.json")
 let fs=require("fs")
 global.fps = 30;
-
 (async () => {
   let express = require("express")
   let app = express()
